97597303e4e2f6734b1a9fb493b20184c937bb49
Tarun Atreya <53280371+Tarun-atreya@users.noreply.github.com>
2023-12-05 13:51:54 -0500

    Part 2

--- Main.java
+++ Main.java
@@ -3,4 +3,17 @@ public class Main{
     {
         int add(int a, int b);
     }
+    public static void main (String[] args)
+    {
+        add a = (int x, int y) -> 
+        {
+            int total = 0;
+            for(int i = x; i <= y; i++)
+            {
+                total += i;
+            }
+            return total;
+        };
+        System.out.println(a.add(1, 4));
+    }
 }
